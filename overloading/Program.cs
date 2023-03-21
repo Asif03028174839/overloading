96641c95b9e53cc8b07a737bde4ee07c917cc5da
Asif <asifm279@gmail.com>
2023-03-21 10:29:16 -0700

    Add project files.

---
+++ overloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+
+    {
+
+        void sum (int a , int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+
+
+        void sum (float a , float b)
+        {
+            Console.WriteLine(a + b);
+        }
+       
+
+        static void Main(string[] args)
+        {
+
+            Program obj = new Program();
+            obj.sum(20.4f, 30.5f);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
